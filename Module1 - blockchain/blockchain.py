43f62ce085c87fa9ad202d5d25c70cb2201e751e
Keshav Bansal <keshavbansal@MacBook-Air.local>
2022-05-11 23:41:11 +0530

    second:

---
+++ Module1 - blockchain/blockchain.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 11 23:14:03 2022
+
+@author: keshavbansal
+"""
+
+import datetime
+import hashlib
+import json 
+from flask import Flask,jsonify
+
+print(2)
+
